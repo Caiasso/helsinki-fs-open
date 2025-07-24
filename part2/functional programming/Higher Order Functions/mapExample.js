f8e6c156d53775d0f6eee0f88eeedca6d161fa01
Caiasso <guilhermecaiasso@gmail.com>
2025-07-23 23:02:10 -0300

    Getting started with functional programming and higher-order functions: Filter and Map methods.

---
+++ part2/functional programming/Higher Order Functions/mapExample.js
@@ -0,0 +1,23 @@
+var animals = [
+  { name: "Fluffykins", species: "rabbit" },
+  { name: "Caro", species: "dog" },
+  { name: "Hamilton", species: "dog" },
+  { name: "Harold", species: "fish" },
+  { name: "Ursula", species: "cat" },
+  { name: "Jummy", species: "fish" },
+];
+
+/* Functional Programming Way */
+var names = animals.map(function (animal) {
+  return animal.name + " is a " + animal.species;
+});
+/* Functional Programming Way */
+console.log(names);
+
+/* The Classic Way */
+var names = [];
+for (var i = 0; i < animals.length; i++) {
+  names.push(animals[i].name);
+}
+/* The Classic Way */
+console.log(names);
